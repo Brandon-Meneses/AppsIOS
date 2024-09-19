e7bbd189a4b4359a68d109a8e9001bef522247ea
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-09-19 16:38:57 -0500

    Initial Commit

---
+++ IMC/IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMCApp.swift
+//  IMC
+//
+//  Created by Brandon Luis Meneses Solorzano on 19/09/24.
+//
+
+import SwiftUI
+
+@main
+struct IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IMCTests/IMCTests.swift
@@ -0,0 +1,17 @@
+//
+//  IMCTests.swift
+//  IMCTests
+//
+//  Created by Brandon Luis Meneses Solorzano on 19/09/24.
+//
+
+import Testing
+@testable import IMC
+
+struct IMCTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
