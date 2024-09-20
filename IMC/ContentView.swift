543b0ef62235a24fc2e5c094bd0d74ee5d9e55fa
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-09-20 16:37:41 -0500

    Toolbar correcta

--- IMC/ContentView.swift
+++ IMC/ContentView.swift
@@ -9,27 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    init() {
-        // Cambiar el color del título de la barra de navegación
-        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
-    }
+//    init() {
+//        // Cambiar el color del título de la barra de navegación
+//        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+//        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor.white]
+//    }
 
     var body: some View {
-        NavigationView {
-            VStack {
-                // Contenido de tu vista
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.backgroundApp) // Reemplaza `.backgroundApp` con un color que definas
-            .navigationTitle("IMC Calculator") // Título de la barra de navegación
-            .navigationBarTitleDisplayMode(.inline) // Muestra el título en el centro
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    
+        VStack{
+            
+        }.frame(maxWidth: .infinity,maxHeight: .infinity)
+            .background(.backgroundApp)
+            .toolbar{
+                ToolbarItem(placement: .principal){
+                    Text("IMC Calculator").bold().foregroundColor(.white)
                 }
             }
-        }
     }
 }
 
